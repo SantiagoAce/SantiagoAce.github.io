41b78ed2e42570ec786833328d89237d5c6910e0
SantiagoAce <45422767+SantiagoAce@users.noreply.github.com>
2019-09-18 22:28:58 +0800

    大致做完了很粗糙的版本，很多细节没做

--- js/index.js
+++ js/index.js
@@ -64,5 +64,135 @@
             slideOn(); //图片淡入
         }
     }
-    
-    slideshow();
+    window.onload = slideshow();
+
+// tab滑动切换
+    // 家电
+    function tabMove() {
+        // 获取鼠标滑过或点击的标签和要切换的内容分类元素
+        var divId01 = document.getElementById('notice-tit');
+        var titles = divId01.getElementsByTagName('li');
+        var divId02 = document.getElementById('notice-con');
+        var divs = divId02.getElementsByClassName('mod');
+        // 检查是否对应
+        if (titles.length != divs.length) return;
+        // 遍历 titles 下所有的li
+        for (var i=0; i<titles.length; i++) {
+            titles[i].id = i;
+            titles[i].onmouseover = function() {
+                // 清除 li 上所有的 class
+                for (var j=0; j<titles.length; j++) {
+                    titles[j].className ='';
+                    divs[j].style.display = 'none';
+                }
+                // 设置当前为高亮显示
+                this.className = 'tab-hot';
+                divs[this.id].style.display = 'block';
+            }
+        }
+    }
+    window.onload = tabMove();
+
+    // 智能
+    function tabMove2() {
+        // 获取鼠标滑过或点击的标签和要切换的内容分类元素
+        var divId01 = document.getElementById('notice-tit2');
+        var titles = divId01.getElementsByTagName('li');
+        var divId02 = document.getElementById('notice-con2');
+        var divs = divId02.getElementsByClassName('mod2');
+        // 检查是否对应
+        if (titles.length != divs.length) return;
+        // 遍历 titles 下所有的li
+        for (var i=0; i<titles.length; i++) {
+            titles[i].id = i;
+            titles[i].onmouseover = function() {
+                // 清除 li 上所有的 class
+                for (var j=0; j<titles.length; j++) {
+                    titles[j].className ='';
+                    divs[j].style.display = 'none';
+                }
+                // 设置当前为高亮显示
+                this.className = 'tab-hot';
+                divs[this.id].style.display = 'block';
+            }
+        }
+    }
+    window.onload = tabMove2();
+
+    // 搭配
+    function tabMove3() {
+        // 获取鼠标滑过或点击的标签和要切换的内容分类元素
+        var divId01 = document.getElementById('notice-tit3');
+        var titles = divId01.getElementsByTagName('li');
+        var divId02 = document.getElementById('notice-con3');
+        var divs = divId02.getElementsByClassName('mod3');
+        // 检查是否对应
+        if (titles.length != divs.length) return;
+        // 遍历 titles 下所有的li
+        for (var i=0; i<titles.length; i++) {
+            titles[i].id = i;
+            titles[i].onmouseover = function() {
+                // 清除 li 上所有的 class
+                for (var j=0; j<titles.length; j++) {
+                    titles[j].className ='';
+                    divs[j].style.display = 'none';
+                }
+                // 设置当前为高亮显示
+                this.className = 'tab-hot';
+                divs[this.id].style.display = 'block';
+            }
+        }
+    }
+    window.onload = tabMove3();
+
+    // 配件
+    function tabMove4() {
+        // 获取鼠标滑过或点击的标签和要切换的内容分类元素
+        var divId01 = document.getElementById('notice-tit4');
+        var titles = divId01.getElementsByTagName('li');
+        var divId02 = document.getElementById('notice-con4');
+        var divs = divId02.getElementsByClassName('mod4');
+        // 检查是否对应
+        if (titles.length != divs.length) return;
+        // 遍历 titles 下所有的li
+        for (var i=0; i<titles.length; i++) {
+            titles[i].id = i;
+            titles[i].onmouseover = function() {
+                // 清除 li 上所有的 class
+                for (var j=0; j<titles.length; j++) {
+                    titles[j].className ='';
+                    divs[j].style.display = 'none';
+                }
+                // 设置当前为高亮显示
+                this.className = 'tab-hot';
+                divs[this.id].style.display = 'block';
+            }
+        }
+    }
+    window.onload = tabMove4();
+
+    // 周边
+    function tabMove5() {
+        // 获取鼠标滑过或点击的标签和要切换的内容分类元素
+        var divId01 = document.getElementById('notice-tit5');
+        var titles = divId01.getElementsByTagName('li');
+        var divId02 = document.getElementById('notice-con5');
+        var divs = divId02.getElementsByClassName('mod5');
+        // 检查是否对应
+        if (titles.length != divs.length) return;
+        // 遍历 titles 下所有的li
+        for (var i=0; i<titles.length; i++) {
+            titles[i].id = i;
+            titles[i].onmouseover = function() {
+                // 清除 li 上所有的 class
+                for (var j=0; j<titles.length; j++) {
+                    titles[j].className ='';
+                    divs[j].style.display = 'none';
+                }
+                // 设置当前为高亮显示
+                this.className = 'tab-hot';
+                divs[this.id].style.display = 'block';
+            }
+        }
+    }
+    window.onload = tabMove5();
